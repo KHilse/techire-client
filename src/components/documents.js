9eecec8b9e7393cc5061e3e6133bdbd6879e5d94
Kirk Hilse <khilsedev@gmail.com>
2019-10-08 08:47:47 -0700

    Added API call for google drive docs

--- src/components/documents.js
+++ src/components/documents.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, send } from 'react';
+import axios from 'axios';
+import SERVER_URL from '../constants';
 
 const Documents = props => {
+
+    const googleToken = localStorage.getItem('googleToken');
+    const [docs, setDocs] = useState([]);
+
+   axios.get('https://www.googleapis.com/drive/v3/files', {
+        headers: {
+            'Authorization': `Bearer ${googleToken}`
+        }
+    })
+    .then(data => {
+        console.log(data);
+    }) 
+    .catch(err => {
+        console.log(err);
+    })     
+
+
+
+    // Axios.get(SERVER_URL + '/documents',{ params: {}, headers: {
+    //     'authorization': googleToken
+    // }},)
+    // .then(response => {
+    //     console.log(response.data);
+    //     setDocs(['Docs go here'])
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    //     setDocs('Failed to get documents');
+    // })
+
+
     return (
         <div className="documents-container">
-            <p>List of documents here</p>
+            <p>{docs}</p>
         </div>
     )
 }
