0d82248a5c3263d217ec6f7e09b0523491068dfc
Kirk Hilse <khilsedev@gmail.com>
2019-10-11 09:59:17 -0700

    Implemented a validator so the contacts list updates on changes

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -14,29 +14,10 @@ const Contacts = props => {
     const [linkedInUrl, setLinkedInUrl] = useState('');
     const [howHelpful, setHowHelpful] = useState('');   
     const [contacts, setContacts] = useState([]);
-
-    //const [addForm, setAddForm] = useState(<></>);
-
+    const [validate, setValidate] = useState(true);
 
     function handleContactFormDisplay() {
         let isVisible = addFormVisible;
-        if (isVisible) { // User clicked the Add button twice, close the form
-            //setAddForm(<></>);
-        } else { // display the form
-            //setAddForm((
-            //     <form id="contact-add-form" onSubmit={handleAddNewContact}>
-            //         <input name="name" type="text" placeholder="Contact name" value={name} onChange={e => setName(e.target.value)} /><br />
-            //         <input name="company" type="text" placeholder="Contact's company" value={company} onChange={e => setCompany(e.target.value)} /><br />
-            //         <input name="jobTitle" type="text" placeholder="Job Title" value={jobTitle} onChange={e => setJobTitle(e.target.value)} /><br />
-            //         <input name="email" type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} /><br />
-            //         <input name="relationship" type="text" placeholder="Your relationship to contact" value={relationship} onChange={e => setRelationship(e.target.value)} /><br />
-            //         <input name="linkedInUrl" type="text" placeholder="LinkedIn URL" value={linkedInUrl} onChange={e => setLinkedInUrl(e.target.value)} /><br />
-            //         <input name="howHelpful" type="text" placeholder="How is this person helpful to you?" value={howHelpful} onChange={e => setHowHelpful(e.target.value)} /><br />
-            //         <input type="submit" value="Add new contact" />
-            //     </form>
-    
-            // ))
-        }
         setAddFormVisible(!isVisible);
     }
 
@@ -52,10 +33,22 @@ const Contacts = props => {
             linkedInUrl: linkedInUrl,
             howHelpful: howHelpful            
         };
-        console.log(formData);
+        // console.log(formData);
         axios.post(`${SERVER_URL}/contacts/new`, formData)
         .then(result => {
+            console.log('Added new contact', result);
+            // Clear form
+            setName('');
+            setCompany('');
+            setJobTitle('');
+            setEmail('');
+            setRelationship('');
+            setLinkedInUrl('');
+            setHowHelpful('');
+            setContacts('');
 
+            setValidate(true);
+            // updateComponent();
         })
         .catch(err => {
             console.log('ERROR adding new contact', err);
@@ -65,36 +58,30 @@ const Contacts = props => {
     function handleDelete(userId, contactId) {
         axios.delete(`${SERVER_URL}/contacts/${userId}/delete/${contactId}`)
         .then(result => {
-            let c = contacts;
-            let cIndex = c.findIndex(i => { return i._id === contactId });
-            if (cIndex >= 0) {
-                c = c.splice(cIndex,1);
-                setContacts(c);
-            }
+            setValidate(true);
+            // updateComponent();
         })
         .catch(err => {
             console.log('ERROR deleting contact', err);
         })
     }
 
-    useEffect(() => {
-        // Get contacts from db through API
+    function updateComponent(cList) {
         axios.get(`${SERVER_URL}/contacts/${props.user._id}`)
         .then(contactList => {
-            console.log('contactList:');
-            console.log(contactList.data);
             setContacts(contactList.data);
-            // let newList = contactList.data.map((c, i) => {
-            //     return (
-            //         <Contact key={i} data={c} handleDelete={handleDelete} />
-            //     )
-            // })
-            // setContacts(newList);
+            setValidate(false);
         })
         .catch(err => {
             console.log('ERROR getting contacts list from API');
-        })
-    },[props.user]);
+        })       
+    }
+    useEffect(() => {
+        // Get contacts from db through API
+        if (validate) {
+            updateComponent();
+        }
+    },[props.user, validate]);
 
     return (
         <div className="contacts-container">
